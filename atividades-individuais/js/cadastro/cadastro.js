cfbbb3f5fa1b9867196e46ad7d2a3f59bdcfa255
Everson Vinicius Cruz Rego <Everson Vinicius Cruz Rego>
2025-10-25 09:36:59 -0300

    cadastro finalizado

--- atividades-individuais/js/cadastro/cadastro.js
+++ atividades-individuais/js/cadastro/cadastro.js
@@ -30,8 +30,21 @@
     let idEmEdicao = null;
 
     const btnCEP = document.querySelector("#btn-buscar-cep");
-
     const inputBusca = document.querySelector("#user-busca");
+
+    //modal
+    const modalDetalhes = document.querySelector("#detalhes-modal");
+    const modalNome = document.querySelector("#modal-nome");
+    const modalEmail = document.querySelector("#modal-email");
+    const modalEndereco = document.querySelector("#modal-endereco-completo");
+    const modalObs = document.querySelector("#modal-obs");
+    const modalBtnEditar = document.querySelector("#modal-btn-editar");
+    const modalBtnExcluir = document.querySelector("#modal-btn-excluir");
+
+    const modal = new bootstrap.Modal(modalDetalhes);
+
+
+
 
     function mostrarTelaLista() {
         telaLista.classList.remove('d-none');
@@ -95,6 +108,7 @@
                 <td>
                     <button type="button" class="btn btn-sm btn-warning" data-id="${user.id}">Editar</button>
                     <button type="button" class="btn btn-sm btn btn-danger" data-id="${user.id}">Excluir</button>
+                    <button type="button" class="btn btn-sm btn btn-primary" data-id="${user.id}">Mostrar Detalhes</button>
                 </td>
             `;
 
@@ -183,8 +197,56 @@
         linkDownload.click();
         URL.revokeObjectURL(url);
     }
-    function uploadArquivo(){
+    function uploadArquivo(event){
+        const arquivo = event.target.files[0];
 
+        if (!arquivo) return;
+
+        const leitor = new FileReader();
+
+        leitor.readAsText(arquivo);
+
+        leitor.onload = function(e){
+            //pega o resultado da leitura do arquivo
+            const conteudoArquivo = e.target.result;
+            
+            const usuariosImportados = JSON.parse(conteudoArquivo);
+
+            if (!Array.isArray(usuariosImportados)){
+                alert("Arquivo não é um array valido!");
+                return;
+            }
+
+            if (confirm("Deseja realmente substituir as informações ds usuários?")){
+                usuarios = usuariosImportados;
+                salvarStorage();
+                renderizarTabela();
+                alert("Usuários importados com sucesso!");
+                inputUpload.value = "";
+                // location.reload(true);
+            }
+        }
+
+    }
+
+    function mostrarDetalhesUsuario (id){
+        const user = usuarios.find(u => u.id ===id);
+
+        if (!user) return;
+
+        modalNome.textContent = `${user.nome} ${user.sobrenome}`;
+        modalEmail.textContent = user.email;
+        const endereco = [user.rua, user.numero, user.complemento, user.bairro, user.cidade, user.estado, user.cep].filter(Boolean).join(", ");
+
+        modalEndereco.textContent = endereco;
+        modalObs.textContent = user.obs;
+
+        modalBtnEditar.dataset.id = user.id;
+        modalBtnExcluir.dataset.id = user.id;
+
+        modal.show();
+
+        
     }
 
 
@@ -212,10 +274,30 @@
             if (isNaN(id)) return
 
             if (target.classList.contains("btn-warning")) {
+
                 editarUsuario(id);
             } else if (target.classList.contains("btn-danger")) {
                 excluirUsuario(id);
-            }
+            } else if (target.classList.contains("btn-primary")) {
+                mostrarDetalhesUsuario(id);
+            } 
+        })
+        modalDetalhes.addEventListener("click", (event) => {
+            const target = event.target.closest("button");
+
+            if (!target) return
+
+            const id = Number(target.dataset.id);
+
+            if (isNaN(id)) return
+
+            if (target.classList.contains("btn-warning")) {
+                modal.hide();
+                editarUsuario(id);
+            } else if (target.classList.contains("btn-danger")) {
+                modal.hide();
+                excluirUsuario(id);
+            } 
         })
     }
     inicializar();
